8aded40e2b7a48bef86aa3aaa25923f55ddedf94
amrelsaid4 <amrelsaid6288@gmail.com>
2024-12-15 17:10:55 +0200

    first commit

---
+++ SMS-project/src/sms/project/SMSProject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sms.project;
+/**
+ *
+ * @author team 14
+ */
+public class SMSProject extends javax.swing.JFrame{
+    public static void main(String[] args) {
+        // TODO code application logic here
+                loginPage login = new loginPage();
+                login.setLocationRelativeTo(null);
+                login.setResizable(false);
+                login.setVisible(true);
+    }
+}
